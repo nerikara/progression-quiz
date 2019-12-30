e246ed49899ecc2c95d52dcba232561be2230627
nerikara <nerikara4chimi@gmail.com>
2019-12-30 23:11:20 +0900

    feature: 数列を表示するページを作成

--- app/controllers/progression_controller.rb
+++ app/controllers/progression_controller.rb
@@ -1,4 +1,7 @@
 class ProgressionController < ApplicationController
   def home
+    @progression = [1, 2, 3, 4]
+    # generator = ProgressionGenerator.new()
+    # @progression = generator.generate(ProgressionType::ARITHMETIC_PROGRESSION).elements
   end
 end
