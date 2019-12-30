8aeaf7499ff8b579ff153e9bd60e24d06a59b543
nerikara <nerikara4chimi@gmail.com>
2019-12-30 23:17:52 +0900

    add: 数列を生成するクラスを追加

---
+++ app/models/progression_generator.rb
@@ -0,0 +1,13 @@
+# 数列を生成するクラス
+class ProgressionGenerator
+
+  include ActiveModel::Model
+
+  # 数列を生成する
+  def generate(type)
+    case type
+    when ProgressionType::ARITHMETIC_PROGRESSION
+      return ArithmeticProgression.new(1, 2)
+    end
+  end
+end
