fb2d2dff2cd2d006179a68a088f7fe3e81791ce1
nerikara <nerikara4chimi@gmail.com>
2019-12-30 18:52:58 +0900

    add: 数列の種別を表す定数を定義したクラスを追加

---
+++ app/models/ProgressionType.rb
@@ -0,0 +1,12 @@
+# 数列の種別を表す定数を定義したクラス
+class ProgressionType
+
+  # 等差数列
+  ARITHMETIC_PROGRESSION.freeze = 0
+
+  # 等比数列
+  GEOMETRIC_PROGRESSION.freeze = 1
+
+  # 階差数列
+  DIFFERENCE_PROGRESSION.freeze = 2
+end
