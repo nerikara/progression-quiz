829e706eeffbbd1a635f2e802dfa43ff8fc74fe7
nerikara <nerikara4chimi@gmail.com>
2019-12-31 22:40:09 +0900

    add: 数列クイズクラスを追加 #13

---
+++ app/models/progression_quiz.rb
@@ -0,0 +1,16 @@
+# 数列クイズのクラス
+class ProgressionQuiz
+
+  attr_reader :values, :target_index
+
+  # コンストラクタ
+  #
+  # @param [Progression] progression 数列
+  def initialize(progression)
+    # 数列
+    @values = progression.elements
+
+    # 隠された数列の項数(0オリジン)
+    @target_index = progression.size - 1
+  end
+end
