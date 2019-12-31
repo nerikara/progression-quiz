881c46761a0c0168833fdd51bdd93d71868a5d29
nerikara <nerikara4chimi@gmail.com>
2019-12-31 13:17:38 +0900

    fix: autoloadの記述を削除(不要なため)

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.autoload_path << Rails.root.join("lib")
-
 module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
