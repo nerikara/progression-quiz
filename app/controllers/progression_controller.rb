9da00f055978503f852671cedec8b92a8debf989
nerikara <nerikara4chimi@gmail.com>
2019-12-31 17:13:32 +0900

    feature: #12 数列をランダムに生成する

--- app/controllers/progression_controller.rb
+++ app/controllers/progression_controller.rb
@@ -2,6 +2,6 @@ class ProgressionController < ApplicationController
   def home
     generator = ProgressionGenerator.new()
     # @progression = generator.generate(ProgressionType::ARITHMETIC_PROGRESSION).elements
-    @progression = generator.generate(ProgressionType::GEOMETRIC_PROGRESSION ).elements
+    @progression = generator.generate(ProgressionType::GEOMETRIC_PROGRESSION).elements
   end
 end
--- app/models/progression_generator.rb
+++ app/models/progression_generator.rb
@@ -3,11 +3,13 @@ class ProgressionGenerator
 
   # 数列を生成する
   def generate(type)
+    first_term = rand(-10..10) + 1    # 0を回避するために+1
+    tolerance = rand(-10..10) + 1     # 0を回避するために+1
     case type
     when ProgressionType::ARITHMETIC_PROGRESSION
-      return ArithmeticProgression.new(1, 2)
+      return ArithmeticProgression.new(first_term, tolerance)
     when ProgressionType::GEOMETRIC_PROGRESSION
-      return GeometricProgression.new(1, 2)
+      return GeometricProgression.new(first_term, tolerance)
     else
       raise "Undefined progression type"
     end
