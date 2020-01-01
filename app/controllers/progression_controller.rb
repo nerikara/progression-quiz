1658642fe5364809819699c4edd4281afc27ad84
nerikara <nerikara4chimi@gmail.com>
2020-01-01 20:38:04 +0900

    add: 空欄と提出ボタンを作成

--- app/controllers/progression_controller.rb
+++ app/controllers/progression_controller.rb
@@ -1,10 +1,13 @@
 class ProgressionController < ApplicationController
+
   def home
     progression_generator = ProgressionGenerator.new()
     progression = progression_generator.generate(ProgressionType::ARITHMETIC_PROGRESSION)
     # progression = progression_generator.generate(ProgressionType::GEOMETRIC_PROGRESSION)
 
     progression_quiz_generator = ProgressionQuizGenerator.new()
-    @progression_quiz = progression_quiz_generator.generate(progression)
+    all_values = progression_quiz_generator.generate(progression).values
+    @hidden_value = all_values.pop
+    @displayed_values = all_values
   end
 end
