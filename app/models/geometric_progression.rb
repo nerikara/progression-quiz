5d2982f0916006eaac63297692941ed717685a4e
nerikara <nerikara4chimi@gmail.com>
2020-02-02 16:46:09 +0900

    等比数列の公比を調整

--- app/models/geometric_progression.rb
+++ app/models/geometric_progression.rb
@@ -5,7 +5,7 @@ class GeometricProgression < Progression
   # @param[Integer] size 数列のサイズ
   def initialize(size)
     @first_term = rand(1..10) * [-1, 1].sample
-    @tolerance = rand(2..3) * [-1, 1].sample
+    @tolerance = rand(2..10) * [-1, 1].sample
     super(size)
   end
 
