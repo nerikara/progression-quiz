12e1c0a7f05e977e5436baa12023e4a08c28e453
nerikara <nerikara4chimi@gmail.com>
2020-02-02 13:06:34 +0900

    リファクタリング

--- app/controllers/progression_controller.rb
+++ app/controllers/progression_controller.rb
@@ -9,10 +9,12 @@ class ProgressionController < ApplicationController
     @correct_answer_count = session[:correct_answer_count].to_i
     @question_count = session[:question_count].to_i
 
+    # 数列生成
     progression_generator = ProgressionGenerator.new()
     @progression_type = params[:progression_type].to_i
     progression = progression_generator.generate(@progression_type)
 
+    # 数列クイズを生成
     progression_quiz_generator = ProgressionQuizGenerator.new()
     all_values = progression_quiz_generator.generate(progression).values
     @hidden_value = all_values.pop
