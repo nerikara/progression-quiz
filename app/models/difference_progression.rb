46af06691a3adcc65363f232760af9d44e186ac0
nerikara <nerikara4chimi@gmail.com>
2020-02-02 16:35:20 +0900

    Progressionクラスおよび子クラスの定義を数学らしく修正

--- app/models/difference_progression.rb
+++ app/models/difference_progression.rb
@@ -5,33 +5,15 @@ class DifferenceProgression < Progression
   # @param [Progression] tolerance_progression 階差数列の公差に使用する数列
   # @param[Integer] size 数列のサイズ
   def initialize(tolerance_progression, size)
-    first_term = rand(1..10) * [-1, 1].sample
-    super(first_term, tolerance_progression, size)
+    @first_term = rand(1..10) * [-1, 1].sample
+    @tolerance_progression = tolerance_progression
+    super(size)
   end
 
-  # 公差
-  # 階差数列の公差は、ベースとなる数列の総和(a1 + a2 + ... + an)
-  # @param [Integer] n 数列のサイズ
-  # @return [Integer] 数列の総和
-  def tolerance(n)
-    if n == 0
-      return @tolerance.elements[0]
-    else
-      return @tolerance.elements[n] + tolerance(n - 1)
-    end
-  end
-
-  # 数列本体
-  # @return [Array] 数列そのもの
-  def elements
-    progression = []
-    @size.times do |i|
-      if i == 0
-        progression.push(@first_term)
-      else
-        progression.push(@first_term + tolerance(i - 1))
-      end
-    end
-    return progression
+  # 一般項
+  # an = a1 + sum( b(n-1) )
+  # @return [Integer] n項目の値
+  def general_term(n)
+    @first_term + (n-1).times.map { |i| @tolerance_progression.elements[i] }.sum
   end
 end
