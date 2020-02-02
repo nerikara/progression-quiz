2394b5c52f586a73c1af2cd303099a29e5c728b4
nerikara <nerikara4chimi@gmail.com>
2020-02-02 15:31:06 +0900

    Progressionクラスおよびサブクラスのコンストラクタを修正

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -4,21 +4,21 @@
 # 継承したクラスでオーバーライドさせることを強制する.
 class Progression
 
-  attr_reader :size
+  attr_reader :first_term, :tolerance, :size
 
-  def initialize
+  # コンストラクタ
+  # @param[Integer] first_term 初項
+  # @param[Any] tolerance 公差
+  # @param[Integer] size 数列のサイズ
+  def initialize(first_term, tolerance, size)
+    # 初項
+    @first_term = first_term
+
+    # 公差
+    @tolerance = tolerance
+
     # 数列のサイズ
-    @size = 5
-  end
-
-  # 初項
-  def first_term
-    raise override_error_message "first_term"
-  end
-
-  # 公差
-  def tolerance
-    raise override_error_message "tolerance"
+    @size = size
   end
 
   # 数列本体
@@ -26,7 +26,9 @@ class Progression
     raise override_error_message "elements"
   end
 
-  # オーバーライドされていないときに出力させるエラーメッセージ
+  # メソッドがオーバーライドされていないときに出力されるエラーメッセージを生成する
+  # @param [String] method_name オーバーライドされていないメソッド名
+  # @return [String] エラーメッセージ
   private def override_error_message(method_name)
     return "abstract method: " + method_name + " should be overriden."
   end
