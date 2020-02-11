eb0685c6fccc5d7c89fadbf2f45411a565001bf2
nerikara <nerikara4chimi@gmail.com>
2020-02-11 15:37:30 +0900

    階差数列を2つ持つ数列を追加

--- app/models/progression_generator.rb
+++ app/models/progression_generator.rb
@@ -6,6 +6,10 @@ class ProgressionGenerator
   PROGRESSION_SIZE.freeze
 
   # 数列を生成する
+  # @param [ProgressionType] type 数列の種類
+  # @return [Progression] 数列
+  # @raises ProgressionTypeが想定されない値である
+  # @raises DIFFERENCE_PROGRESSION_COUNTの値が想定されない値である
   def generate(type)
     case type
     when ProgressionType::ARITHMETIC_PROGRESSION
@@ -13,9 +17,21 @@ class ProgressionGenerator
     when ProgressionType::GEOMETRIC_PROGRESSION
       return GeometricProgression.new(PROGRESSION_SIZE)
     when ProgressionType::PROGRESSION_WITH_DIFFERENCE_PROGRESSION
-      difference_progression = generate([ProgressionType::ARITHMETIC_PROGRESSION,
-                                         ProgressionType::GEOMETRIC_PROGRESSION].sample)
-      return ProgressionWithDifferenceProgression.new(difference_progression, PROGRESSION_SIZE)
+      difference_progression_count = [1, 2].sample
+      difference_progression_count.freeze
+      case difference_progression_count
+      when 1
+        difference_progression = generate([ProgressionType::ARITHMETIC_PROGRESSION,
+                                          ProgressionType::GEOMETRIC_PROGRESSION].sample)
+        return ProgressionWithDifferenceProgression.new(difference_progression, PROGRESSION_SIZE)
+      when 2
+        difference_progression_1 = generate([ProgressionType::ARITHMETIC_PROGRESSION,
+                                          ProgressionType::GEOMETRIC_PROGRESSION].sample)
+        difference_progression_2 = ProgressionWithDifferenceProgression.new(difference_progression_1, PROGRESSION_SIZE)
+        return ProgressionWithDifferenceProgression.new(difference_progression_2, PROGRESSION_SIZE)
+      else
+        raise "difference_progression_count == '#{difference_progression_count}' "
+      end
     when ProgressionType::FIBONACCI_PROGRESSION
       return FibonacciProgression.new(PROGRESSION_SIZE)
     when ProgressionType::TRIBONACCI_PROGRESSION
